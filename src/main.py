6a5507d4b5e0b814d43cf2ddd48ba50476eed8e6
rianexists <rconnorwebster@gmail.com>
2024-11-20 10:43:30 +1100

    Chapter 2

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from textnode import TextNode, TextType
 
+
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "boot.dev")
-    print(node.__repr__())
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
 
 main()
