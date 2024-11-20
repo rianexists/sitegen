491588de1b7217e399942c2bc6f49e659bdb97bd
rianexists <rconnorwebster@gmail.com>
2024-11-20 11:06:43 +1100

    Chapter 3

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -52,6 +52,5 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.value, "This is bold")
 
-
 if __name__ == "__main__":
     unittest.main()
